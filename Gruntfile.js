b96a582c02bc7b7e80b4d9fe8ed2d8b7e9228d3e
loddit <loddit@gmail.com>
2016-01-20 11:16:19 +0800

    add grunt-marked task

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+'use strict';
+
+// # Globbing
+// for performance reasons we're only matching one level down:
+// 'test/spec/{,*/}*.js'
+// use this if you want to recursively match all subfolders:
+// 'test/spec/**/*.js'
+
+module.exports = function (grunt) {
+
+  // Load grunt tasks automatically
+  require('load-grunt-tasks')(grunt);
+
+  // Define the configuration for all the tasks
+  grunt.initConfig({
+    marked: {
+      dist: {
+        options: {
+          highlight: false
+        },
+        files: [
+          {
+            expand: true,     // Enable dynamic expansion.
+            cwd: 'tutorials/markdown',      // Src matches are relative to this path.
+            src: ['**/*.md'], // Actual pattern(s) to match.
+            dest: 'dist/html',   // Destination path prefix.
+            ext: '.html',   // Dest filepaths will have this extension.
+          },
+        ],
+      }
+    },
+  });
+
+  grunt.registerTask('build', [
+    'marked:dist'
+  ]);
+};
